b06d4584fd8e174d2adb91517d022ef5f9cce1e5
Adrian Maulani <takiya2888157@gmail.com>
2023-12-13 21:30:09 +0700

    Adds convenience to use whether Uppercased/Lowercased components

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,16 @@ const pages = import.meta.glob("./pages/**/*.jsx");
 document.addEventListener("DOMContentLoaded", () => {
   createInertiaApp({
     resolve: async (name) => {
-      const page = (await pages[`./pages/${name}.jsx`]()).default;
+      const pageName = name.charAt(0).toUpperCase() + name.slice(1);
+      let page;
+
+      try {
+        page = (await pages[`./pages/${pageName}.jsx`]()).default;
+      }
+      catch {
+        page = (await pages[`./pages/${name}.jsx`]()).default;
+      }
+
       page.layout = page.layout;
       return page;
     },
