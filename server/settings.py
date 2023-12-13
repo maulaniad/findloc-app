8bfa7b12a37743d883d718d29a2e273001de3ff4
Adrian Maulani <takiya2888157@gmail.com>
2023-12-13 17:09:44 +0700

    Renamed the Django project folder

--- server/settings.py
+++ server/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'server.wsgi.application'
 
 
 # Database
